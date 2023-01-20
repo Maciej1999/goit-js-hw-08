6408a3a0023675b1c294a46154c07e2d63f59ffe
Maciej1999 <maciej.sikora.1999@wp.pl>
2023-01-20 12:27:15 +0100

    end of hw-js-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
-// Add imports above this line
+import _ from 'lodash';
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+for (let i = 0; i < galleryItems.length; i++) {
+  const link = document.createElement('a');
+  link.className = 'gallery__item';
+  link.href = `${galleryItems[i].original}`;
+  const img = document.createElement('img');
+  img.className = 'gallery__image';
+  img.src = galleryItems[i].preview;
+  img.alt = galleryItems[i].description;
+  link.appendChild(img);
+  gallery.appendChild(link);
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import _ from 'lodash';
+const form = document.querySelector('.feedback-form');
 
+window.addEventListener('load', () => {
+  const valueJSON = localStorage.getItem('feedback-form-state');
+  const data = JSON.parse(valueJSON);
+  try {
+    Array.from(form.elements).forEach(el => {
+      if (el.name === 'email') {
+        el.value = data.email;
+      }
+      if (el.name === 'message') {
+        el.value = data.message;
+      }
+    });
+  } catch (error) {}
+});
+
+form.addEventListener(
+  'input',
+  _.throttle(e => {
+    e.preventDefault();
+    let data = {};
+    Array.from(form.elements).forEach(el => {
+      if (el.name === 'email') {
+        data.email = el.value;
+      }
+      if (el.name === 'message') {
+        data.message = el.value;
+      }
+    });
+    const dataJSON = JSON.stringify(data);
+    localStorage.setItem('feedback-form-state', dataJSON);
+  }, 500)
+);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  Array.from(form.elements).forEach(el => {
+    if (el.name === 'email' || el.name === 'message') {
+      el.value = '';
+    }
+  });
+  const valueJSON = localStorage.getItem('feedback-form-state');
+  const data = JSON.parse(valueJSON);
+  console.log(data);
+  localStorage.clear();
+});
